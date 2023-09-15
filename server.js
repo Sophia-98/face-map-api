2067fe9fb26184f3c3be64c45ee63fcc99533f1c
Sophia Panagiotaras <sophiapanagiotaras31@gmail.com>
2023-09-15 18:10:56 +0100

    url changes

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'sophia',
-      password : 'post123',
-      database : 'facerecogdb'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
@@ -40,6 +37,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
 
-app.listen(3000, ()=> {
-    console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`App is running ${process.env.PORT}`);
 })
